604622a7c3ab4d1b928b0e8be0e192939e242865
Brad Bollenbach <bradb@30sleeps.com>
2022-12-14 21:26:58 +0000

    part 2

--- problem_1.clj
+++ problem_1.clj
@@ -1,19 +1,34 @@
 (ns problem-1
   (:require [clojure.string :as str]))
 
+(defn input->calorie-totals
+  [input]
+  (->> input
+       slurp
+       str/split-lines
+       (partition-by str/blank?)
+       (reduce (fn [acc [x & _ :as xs]]
+                 (if (seq x)
+                   (conj acc (apply + (map #(Integer/parseInt %) xs)))
+                   acc))
+               [])))
+
 (defn max-cals
-  []
-  (->> "data/problem-1-1.txt"
-      slurp
-      str/split-lines
-      (partition-by str/blank?)
-      (reduce (fn [acc [x & _ :as xs]]
-                (if (seq x)
-                  (conj acc (apply + (map #(Integer/parseInt %) xs)))
-                  acc))
-              [])
-      (apply max)))
+  [input]
+  (->> input
+       input->calorie-totals
+       (apply max)))
+
+(defn top-n-cals
+  [input n]
+  (let [sorted-cals (->> input
+                         input->calorie-totals
+                         sort
+                         reverse)]
+    (->> (take n sorted-cals)
+         (apply +))))
 
 (comment
-  (max-cals))
+  (let [input "data/problem-1-1.txt"]
+    (top-n-cals input 3)))
 

0a85898bc3c0dc8ce92d50527da0d01728419ddf
Brad Bollenbach <bradb@30sleeps.com>
2022-12-17 20:41:53 +0000

    day 5 parsing

---
+++ day_5.clj
@@ -0,0 +1,40 @@
+(ns day-5
+  (:require [clojure.java.io :as io]
+            [portal.api :as portal]))
+
+(def ^:private stack-row-re #"\[")
+(def ^:private stack-numbers-re #"^\s*1")
+(def ^:private instruction-re #"\s*move")
+
+(defn tops
+  [input]
+  (let [parsed
+        (reduce (fn parse
+                  [acc row]
+                  (cond
+                    (re-seq stack-row-re row)
+                    (update acc :stacks conj row)
+
+                    (re-seq stack-numbers-re row)
+                    (update acc :stack-numbers conj row)
+
+                    (re-seq instruction-re row)
+                    (update acc :instructions conj row)
+
+                    :else
+                    acc))
+                {:stacks [], :stack-numbers [], :instructions []}
+                input)]
+    (tap> parsed)))
+
+(comment
+  (portal/open)
+  (add-tap #'portal/submit)
+
+  (with-open [rdr (-> "day-5.txt"
+                      io/resource
+                      io/reader)]
+    (portal/clear)
+    (-> rdr
+        line-seq
+        tops)))

20e9395da6ba2a3bdce0f3e7e36757d0fe4df5e1
Brad Bollenbach <bradb@30sleeps.com>
2022-12-17 09:39:49 +0000

    problem 3, part 1

---
+++ problem_3.clj
@@ -0,0 +1,28 @@
+(ns problem-3
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn sum-priorities
+  [rucksacks]
+  (reduce (fn sum-priorities
+            [acc rucksack-contents]
+            (let [[s1 s2]
+                  (->> rucksack-contents
+                       (split-at (/ (count rucksack-contents) 2))
+                       (map set))
+
+                  dupe-int (-> (set/intersection s1 s2)
+                           first
+                           int)]
+              (if (>= dupe-int 97)
+                (+ acc (- dupe-int 96))
+                (+ acc (- dupe-int 38)))))
+          0
+          rucksacks))
+
+(comment
+  (with-open [rdr (-> "problem-3-1.txt"
+                      io/resource
+                      io/reader)]
+    (sum-priorities (line-seq rdr))))

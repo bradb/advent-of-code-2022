de4ab2636447fe54920d323c276ce80f72205f76
Brad Bollenbach <bradb@30sleeps.com>
2022-12-17 16:55:39 +0000

    day 4, part 1

---
+++ day_4.clj
@@ -0,0 +1,34 @@
+(ns day-4
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn count-full-overlaps
+  [input]
+  (reduce (fn count-ols
+            [acc row]
+            (let [[left-start left-end right-start right-end]
+                  (->> (str/split row #",")
+                       (map #(str/split % #"-"))
+                       flatten
+                       (map #(Integer/parseInt %)))
+                  left-sections (-> (range left-start (inc left-end))
+                                    set)
+                  right-sections (-> (range right-start (inc right-end))
+                                     set)
+                  overlap (set/intersection left-sections right-sections)]
+              (if (or (= overlap left-sections)
+                      (= overlap right-sections))
+                (inc acc)
+                acc)))
+          0
+          input))
+
+(comment
+  (with-open [rdr (-> "day-4.txt"
+                      io/resource
+                      io/reader)]
+    (-> rdr
+        line-seq
+        count-full-overlaps)))
+

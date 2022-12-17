1ed539ecc96c6128ebb98794911fac5e3b1cb9ae
Brad Bollenbach <bradb@30sleeps.com>
2022-12-17 16:58:37 +0000

    day 4, part 2

--- day_4.clj
+++ day_4.clj
@@ -24,11 +24,31 @@
           0
           input))
 
+(defn count-overlaps
+  [input]
+  (reduce (fn count-ols
+            [acc row]
+            (let [[left-start left-end right-start right-end]
+                  (->> (str/split row #",")
+                       (map #(str/split % #"-"))
+                       flatten
+                       (map #(Integer/parseInt %)))
+                  left-sections (-> (range left-start (inc left-end))
+                                    set)
+                  right-sections (-> (range right-start (inc right-end))
+                                     set)
+                  overlap (set/intersection left-sections right-sections)]
+              (if (seq overlap)
+                (inc acc)
+                acc)))
+          0
+          input))
+
 (comment
   (with-open [rdr (-> "day-4.txt"
                       io/resource
                       io/reader)]
     (-> rdr
         line-seq
-        count-full-overlaps)))
+        count-overlaps)))
 
